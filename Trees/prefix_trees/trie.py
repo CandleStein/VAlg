d54c99548d8ab18de351256165f510d65569bf49
stellarator-x <admathur@iitk.ac.in>
2020-10-09 16:52:13 +0530

    adding prefix trees

---
+++ Trees/prefix_trees/trie.py
@@ -0,0 +1,140 @@
+# Module for prefix trees
+import pickle
+import re
+import dict
+import copy
+
+
+def isWord(word):
+    for ch in word:
+        if not ("a" <= ch.lower() <= "z"):
+            return False
+    return dict.check(word)
+
+
+class node(object):
+    def __init__(self, val):
+        self.val = val
+        self.children = []
+        self.isend = False
+
+    def add_child(self, child):
+        self.children.append(child)
+
+    def get_val(self):
+        return self.val
+
+
+class trie(object):
+    def __init__(self):
+        self.root = node(None)
+
+    def add(self, word):
+        if (len(word) < 4) or not (isWord(word)):
+            return None
+        word = word.lower()
+        curr = self.root
+        for i, a in enumerate(word):
+            if a not in [child.val for child in curr.children]:
+                curr.children.append(node(a))
+                curr = curr.children[-1]
+            else:
+                for j, val in enumerate([child.val for child in curr.children]):
+                    if val == a:
+                        curr = curr.children[j]
+                        break
+            if i == len(word) - 1:
+                curr.isend = True
+        return True
+
+    def get_vocab(self, node, word="", words=[]):
+        if len(word):
+            assert node != self.root
+        if len(node.children) == 0:
+            words.append(word)
+        else:
+            if node.isend:
+                words.append(word)
+            for child in node.children:
+                self.get_vocab(child, word + child.val, words)
+        return words
+
+    def print_(
+        self, start_node
+    ):  # simply printing all the letters in the vocab for now
+        for child in start_node.children:
+            self.print_(child)
+        if start_node.val != None:
+            print(start_node.val)
+
+    def print_vocab(
+        self, node, first=False, word=""
+    ):  # printing all the words in the vocabulary
+        # We need a designation for where the word ends as well.
+        if first:
+            print("My vocab presently contains:")
+        # Base Case:
+        if len(node.children) == 0:
+            print(word)
+        else:
+            if node.isend == True:
+                print(word)
+            for child in node.children:
+                self.print_vocab(child, first=False, word=word + child.val)
+
+    def search_word(self, word):
+        pass
+
+    def words_with(self, pre):
+        # finding the
+        curr = copy.deepcopy(self.root)
+        for char in pre:
+            c = [node.val for node in curr.children]
+            if char in c:
+                curr = curr.children[c.index(char)]
+            else:
+                print("No such words in this vocab.")
+                return
+        self.print_vocab(curr, False, pre)
+
+    def get_words_with(self, pre):
+        curr = self.root
+        for char in pre:
+            c = [node.val for node in curr.children]
+            if char in c:
+                curr = curr.children[c.index(char)]
+            else:
+                print("No such words in this vocab.")
+                return
+        return self.get_vocab(node=curr, word=pre, words=[])
+
+    def clear(self):
+        for child in self.root.children:
+            self.root.children.remove(child)
+
+    def purge_errors(self):
+        for child in self.root.children:
+            if child.isend:
+                if child.val not in ["i", "a"]:
+                    child.isend = False
+
+
+def clean(string):
+    for word in string:
+        if len(word) == 1:
+            string.remove(word)
+    return string
+
+
+if __name__ == "__main__":
+    vocab = trie()
+    text = open("Resources/tester.txt", "r+")
+    text = text.read()
+    words = re.split(r"\W+", text)
+    for word in words:
+        vocab.add(word)
+    vocab.purge_errors()
+    voc = vocab.get_vocab(vocab.root)
+    voc_w_swi = vocab.get_words_with(pre="s")
+    print(voc_w_swi)
+    vocab.words_with("s")
